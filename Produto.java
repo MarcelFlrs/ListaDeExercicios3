d8e842176102959b0c7b3179c3d4998c44c36de4
Marcel <127535882+MarcelFlrs@users.noreply.github.com>
2024-03-31 18:40:34 -0300

    Exercício 06

--- Produto.java
+++ Produto.java
@@ -1,12 +1,10 @@
 public class Produto {
     public double precoDeCusto;
-    public double acrescimo;
-    public double porcentagem;
+    public double porcentagemAcrescimo;
     public double valorVenda;
 
     public void calcValorVenda(){
-        porcentagem = acrescimo * 0.1;
-        valorVenda = precoDeCusto + (precoDeCusto * porcentagem);
+        valorVenda = precoDeCusto + (precoDeCusto * porcentagemAcrescimo);
     }
 }
 
