0811da5f883c4c75b6d16d8d3710e3c13b6546a6
Marcel <127535882+MarcelFlrs@users.noreply.github.com>
2024-03-31 19:06:54 -0300

    Exercício 06

--- Exercicio06.java
+++ Exercicio06.java
@@ -2,7 +2,7 @@ public class Exercicio06 {
     public static void executar() {
         Produto produto1 = new Produto();
         produto1.precoDeCusto = Prompt.lerDecimal("Digite o preço de custo do produto: ");
-        produto1.porcentagemAcrescimo = Prompt.lerDecimal("Digite a porcentagem de acréscimo: ")* 0.1;
+        produto1.porcentagemAcrescimo = Prompt.lerDecimal("Digite a porcentagem de acréscimo: ")* 0.01;
         produto1.calcValorVenda();
 
         System.out.printf("O valor da venda do produto é: %.1f", produto1.valorVenda);
